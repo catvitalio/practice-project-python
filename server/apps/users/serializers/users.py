3fde5b82a81c5d45d18967cdaf39e6e62d014437
catvitalio <catvitalio@protonmail.com>
2020-07-23 12:56:27 +0700

    code formatting fixed

--- server/apps/users/serializers/users.py
+++ server/apps/users/serializers/users.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import SerializerMethodField
 
 
 class UserSerializer(ModelSerializer):
+    token = SerializerMethodField()
+
+    def get_token(self, instance):
+        token = Token.objects.get(user=instance).key
+        return token
+
     class Meta:
         model = User
-        fields = ('username', 'password', )
+        fields = ('id', 'username', 'password', 'token', )
+        extra_kwargs = {'password': {'write_only': True}}
