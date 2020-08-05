d41cc46736a71c0bf75bd170d156206752940e64
catvitalio <catvitalio@protonmail.com>
2020-08-05 14:57:29 +0700

    avg, sum signals reversed

--- server/apps/eats/models/signals/calculate_calories.py
+++ server/apps/eats/models/signals/calculate_calories.py
@@ -1,10 +1,14 @@
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
+from django.db.models import Sum
 
 from apps.eats.models import Dish
+from apps.eats.models import Ingredient
 
 
 @receiver(m2m_changed, sender=Dish.ingredients.through)
 def calculate_calories(sender, instance, **kwargs):
-    instance.calories = Dish.objects.with_sum_calories(instance.id)
+    calories_sum = Ingredient.objects.filter(
+        dish__id__exact=instance.id).aggregate(Sum('calories'))
+    instance.calories = calories_sum['calories__sum']
     instance.save()
