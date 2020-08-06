14576bb9c2b358f2274d70eb360ca870b2582260
catvitalio <catvitalio@protonmail.com>
2020-08-06 16:48:54 +0700

    permissions fixed

--- server/apps/main/permissions/main.py
+++ server/apps/main/permissions/main.py
@@ -1,35 +1,40 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import permissions
+from rest_framework.exceptions import APIException
 
 from apps.eats.models import Place
 
 
 class PlacePermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated
+        return (
+                request.method in permissions.SAFE_METHODS or
+                request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        return bool(
-            request.method in permissions.SAFE_METHODS or
-            obj.owner == request.user
+        return (
+                request.method in permissions.SAFE_METHODS or
+                obj.owner == request.user
         )
 
 
 class DishPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST' and request.user.is_authenticated:
-            owner = Place.objects.values_list(
-                'owner', flat=True).get(id=request.data['place'])
-            return request.user.id == owner
-        return bool(
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated
+            try:
+                owner = Place.objects.get(id=request.data['place']).owner
+                return request.user == owner
+            except ObjectDoesNotExist:
+                raise APIException("Выбранного заведения нет в списке")
+
+        return (
+                request.method in permissions.SAFE_METHODS or
+                request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        return bool(
-            request.method in permissions.SAFE_METHODS or
-            obj.place.owner == request.user
+        return (
+                request.method in permissions.SAFE_METHODS or
+                obj.place.owner == request.user
         )
